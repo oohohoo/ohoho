3e35b5231ef5baf50f269e6b4d598f50b224c7a9
dejan-77 <dejankutic@me.com>
2021-01-30 18:19:21 +0100

    feji

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-function setUpCanvas() {
-  canvas = document.getElementsByClassName("canvas_hangman")[0];
-  ctx = canvas.getContext('2d');
-  ctx.translate(0.5, 0.5);
 
-  // Set display size (vw/vh).
-  var sizeWidth = 80 * window.innerWidth / 100,
-    sizeHeight = 100 * window.innerHeight / 100 || 766;
-
-  //Setting the canvas site and width to be responsive 
-  canvas.width = sizeWidth;
-  canvas.height = sizeHeight;
-  canvas.style.width = sizeWidth;
-  canvas.style.height = sizeHeight;
-}
-
-window.onload = setUpCanvas();
 
 
 
@@ -313,6 +297,25 @@ init = function() {
 
 init();
 /*canvas.style.zIndex = 99;*/
+
+
+function setUpCanvas() {
+  canvas = document.getElementsByClassName("canvas_hangman")[0];
+  ctx = canvas.getContext('2d');
+  ctx.translate(0.5, 0.5);
+
+  // Set display size (vw/vh).
+  var sizeWidth = 80 * window.innerWidth / 100,
+    sizeHeight = 100 * window.innerHeight / 100 || 766;
+
+  //Setting the canvas site and width to be responsive 
+  canvas.width = sizeWidth;
+  canvas.height = sizeHeight;
+  canvas.style.width = sizeWidth;
+  canvas.style.height = sizeHeight;
+}
+
+window.onload = setUpCanvas();
 
 
 /*==================================
