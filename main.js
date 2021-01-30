3683fd2ed047d12b6f18a00e8a1c9e5d4aa0bf66
dejan-77 <dejankutic@me.com>
2021-01-30 18:18:07 +0100

    fji

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function setUpCanvas() {
+  canvas = document.getElementsByClassName("canvas_hangman")[0];
+  ctx = canvas.getContext('2d');
+  ctx.translate(0.5, 0.5);
+
+  // Set display size (vw/vh).
+  var sizeWidth = 80 * window.innerWidth / 100,
+    sizeHeight = 100 * window.innerHeight / 100 || 766;
+
+  //Setting the canvas site and width to be responsive 
+  canvas.width = sizeWidth;
+  canvas.height = sizeHeight;
+  canvas.style.width = sizeWidth;
+  canvas.style.height = sizeHeight;
+}
+
+window.onload = setUpCanvas();
+
+
+
 /*
 ===
 CANVAS BLOB
