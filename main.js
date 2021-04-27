661f1b3faa377a738b3ecdf1fad80b1d7e6c6fbc
dejan-77 <dejankutic@me.com>
2021-04-27 15:51:17 +0200

    f4

--- main.js
+++ main.js
@@ -332,7 +332,7 @@ RESIZE CANVAS IN VW
 */
 
 function setUpCanvas() {
-  canvas = document.getElementsByClassName("canvas_hangman")[0];
+  canvas = document.getElementsByClassName("blob-wrap")[0];
   ctx = canvas.getContext('2d');
   ctx.translate(0.5, 0.5);
 
