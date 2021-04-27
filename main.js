80ba55e680655a7eb55840adbf6e3595ed55962c
dejan-77 <dejankutic@me.com>
2021-04-27 18:34:42 +0200

    gr

--- main.js
+++ main.js
@@ -364,7 +364,7 @@ canvas.height = scr.h
 RESIZE CANVAS IN VW
 ====================================
 */
-
+/*
 function setUpCanvas() {
   canvas = document.getElementsByClassName("blob-wrapper")[0];
  ctx = canvas.getContext('2d');
