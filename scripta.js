ba9d103bfa393e93a004649c61c306bc5217d1ca
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-02-08 21:07:56 +0100

    gr

--- scripta.js
+++ scripta.js
@@ -21,10 +21,10 @@ let tween = gsap.to(button, {
 });
 
 button.addEventListener('mouseenter', () => {
-  gsap.to(tween, {duration: 1});
+  gsap.to(tween, {duration: 1, time: tween.duration(), ease: 'power3.inOut'});
 });
 button.addEventListener('mouseleave', () => {
-  gsap.to(tween, {duration: 2, delay: 3, clearProps:"all", overwrite: true});
+  gsap.to(tween, {duration: 2, delay: 3, time: 0, ease: 'none', clearProps:"all", overwrite: true});
 });
 
 });
