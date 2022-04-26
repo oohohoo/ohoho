686a6e44dd517735b3631fb6f47ac8aa614a24a5
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-26 19:02:38 +0200

    hh

--- ohoho-app.js
+++ ohoho-app.js
@@ -88,11 +88,11 @@ MATTER.JS
 
         // resize O
         function percentX(percent) {
-            return Math.round(9.9 / 100 * window.innerWidth);
+            return Math.round(9.8 / 100 * window.innerWidth);
         }
         // resize H
         function percentY(percent) {
-            return Math.round(19.4 / 100 * window.innerWidth);
+            return Math.round(19.3 / 100 * window.innerWidth);
         }
 
         // ADD OBJECTS
