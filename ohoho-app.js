5f83b1b35fc9bfeb11b6e3a52dfc5a20261338ac
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-26 18:03:00 +0200

    f

--- ohoho-app.js
+++ ohoho-app.js
@@ -256,7 +256,7 @@ Matter.Events.on(engine, 'beforeUpdate', function(event) {
     var engine = event.source;
 
     // apply random forces every 5 secs
-    if (event.timestamp % 5000 < 50)
+    if (event.timestamp % 15000 < 50)
         shakeScene(engine);
 });
 
