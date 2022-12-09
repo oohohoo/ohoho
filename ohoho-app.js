a595f8840ed56b0b70c352aef40fbf2bb6a64e4f
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-12-09 17:21:42 +0100

    gr

--- ohoho-app.js
+++ ohoho-app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+/* window.addEventListener('load', function () { */
 
 /* 
 =============================================  
@@ -515,7 +515,7 @@ CANVAS RESIZE
 
     draw(); */
 
-});
+/* }); */
 
     // Get current view sizes
 let viewSizes = [canvas.width = window.innerWidth, canvas.height = window.innerHeight];
