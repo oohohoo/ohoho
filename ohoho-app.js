d1deddeb92116d06857a8ddafbbfebbd6f311f4f
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-12-20 06:20:33 +0100

    f

--- ohoho-app.js
+++ ohoho-app.js
@@ -489,36 +489,38 @@ window.addEventListener('load', function () {
     =============================================
     */
     
-   // dodaj event listener za startdrag događaj na mouseConstraint objektu
-Matter.Events.on(mouseConstraint, 'startdrag', function (event) {
-    // provjeri da li se miš nalazi iznad nekog objekta
-    const bodies = Matter.Query.point(Matter.Composite.allBodies(world), event.mouse.position);
+    // START DRAG
+    Matter.Events.on(mouseConstraint, 'startdrag', function (event) {
+        const bodies = Matter.Query.point(Matter.Composite.allBodies(world), event.mouse.position);
+      
+        bodies.forEach(b => {
+          b.render.fillStyle = linkColor;
+        })
+    });
+      
+    // END DRAG
+    Matter.Events.on(mouseConstraint, 'enddrag', function (event) {
+        const bodies = Matter.Query.point(Matter.Composite.allBodies(world), event.mouse.position);
+      
+        bodies.forEach(b => {
+          b.render.fillStyle = textColor;
+        })
+    });
+
+    // dodaj event listener za mousemove događaj na canvas element
+canvas.addEventListener('mousemove', function(event) {
+    // dohvati poziciju miša na canvasu
+    var mousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+  
+    // koristi Matter.Query.point da provjeri da li se miš nalazi iznad nekog objekta
+    var bodies = Matter.Query.point(Matter.Composite.allBodies(world), mousePosition);
   
     if (bodies.length > 0) {
       // ako se miš nalazi iznad nekog objekta, promijeni kursor na pokazivač (pointer)
       canvas.style.cursor = 'pointer';
-      // mijenjaj boju objekta u linkColor
-      bodies.forEach(b => {
-        b.render.fillStyle = linkColor;
-      });
-    } else {
-      // u suprotnom, vrati kursor na zadani (default)
-      canvas.style.cursor = 'default';
-    }
-  });
-  
-  // dodaj event listener za enddrag događaj na mouseConstraint objektu
-  Matter.Events.on(mouseConstraint, 'enddrag', function (event) {
-    // provjeri da li se miš nalazi iznad nekog objekta
-    const bodies = Matter.Query.point(Matter.Composite.allBodies(world), event.mouse.position);
-  
-    if (bodies.length > 0) {
-      // ako se miš nalazi iznad nekog objekta, promijeni kursor na pokazivač (pointer)
-      canvas.style.cursor = 'pointer';
-      // mijenjaj boju objekta u textColor
-      bodies.forEach(b => {
-        b.render.fillStyle = textColor;
-      });
     } else {
       // u suprotnom, vrati kursor na zadani (default)
       canvas.style.cursor = 'default';
